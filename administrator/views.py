af7344cbad99768e9c5a9ca7568f47fcb6217981
Gibson Dignity <gibsondignty@gmail.com>
2024-03-05 11:26:32 +0000

    administrator view

--- administrator/views.py
+++ administrator/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def admin_login(request):
     if request.method:
-        print("Request has gone through")
+        
     return render(request, 'administrator/login.html')
 
 
