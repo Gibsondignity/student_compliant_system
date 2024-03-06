33ec9d1b8f1106b4808b781c7d1e2776db48c896
Gibsondignity <64210963+Gibsondignity@users.noreply.github.com>
2024-03-06 04:03:33 +0000

    super user views

--- administrator/views.py
+++ administrator/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 
+from users.forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout #add this
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render,redirect, reverse
+
 # Create your views here.
 def admin_login(request):
-    # if request.method:
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+                if user.is_superuser:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}.")
+                    return redirect(reverse('home'))
+                messages.error(request,"Sorry! You do not have an admin privileges. Login as a staff.")
+                return render(request, 'administrator/login.html', context={"form":form})
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    
         
-    return render(request, 'administrator/login.html')
+    return render(request, 'administrator/login.html', context={"form":form})
 
 
